0fa1074848e07825a08b8363614a323e5f1f9d63
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-01-27 22:14:00 +0200

    Updated friend mode by implementing scoring system

---
+++ functions.py
@@ -0,0 +1,180 @@
+from random import random, randrange
+
+def input_validation(prompt, min_val, max_val):
+    while True:
+        try:
+            num = int(input(prompt))
+            if min_val <= num <= max_val:
+                return num
+            else:
+                print(f'\nPlease enter a valid option({min_val}-{max_val})')
+        except ValueError:
+            print(f'\nPlease enter a valid option({min_val}-{max_val})')
+
+
+def print_board(board):
+    print('\n    1   2   3')
+    print('  +---+---+---+ ')
+    for i in range(3):
+        print(f'{i+1} | {board[i][0]} | {board[i][1]} | {board[i][2]} |')
+        print('  +---+---+---+')
+    print('')
+
+
+def print_score(score1, score2):
+    
+    print(f'\n| Player X: {score1}')
+    print(f'| Player O: {score2}')
+
+
+def reset_board():
+    return [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']]
+
+
+def reset_score(stats):
+    stats['friend']['X'] = 0
+    stats['friend']['O'] = 0
+    stats['friend']['round'] = 1
+
+
+def check_row(board, row):
+    return (board[row][0] == board[row][1] == board[row][2]) and board[row][0] != ' '
+
+
+def check_column(board, column):
+    return (board[0][column] == board[1][column] == board[2][column]) and board[0][column] != ' '
+
+
+def check_diagonals(board):
+    return ((board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ')
+            or (board[2][0] == board[1][1] == board[0][2] and board[2][0] != ' '))
+
+
+def computer_move(board, level, player_symbol, computer_symbol):
+
+    def check_threat(sequence, symbol):
+        cnt = 0
+        empty = None
+        for pos in sequence:
+            if board[pos[0]][pos[1]] == symbol:
+                cnt += 1
+            elif board[pos[0]][pos[1]] == ' ':
+                empty = pos
+
+        if cnt == 2 and empty is not None:
+            return empty
+        return None
+
+
+    def danger(symbol):
+        for i in range(3):
+            row = [(i, j) for j in range(3)]
+            pos = check_threat(row, symbol)
+            if pos is not None:
+                return pos
+
+        for j in range(3):
+            column = [(i, j) for i in range(3)]
+            pos = check_threat(column, symbol)
+            if pos is not None:
+                return pos
+
+        pos = check_threat([(0, 0), (1, 1), (2, 2)], symbol)
+        if pos is not None:
+            return pos
+
+        pos = check_threat([(0, 2), (1, 1), (2, 0)], symbol)
+        if pos is not None:
+            return pos
+
+
+    if level == 1:
+        pos = danger(computer_symbol)
+        if pos is not None and random() < 0.25:
+            return pos
+        pos = danger(player_symbol)
+        if pos is not None and random() < 0.25:
+            return pos
+        
+        empty_squares = [] 
+        for i in range(3): 
+            for j in range(3): 
+                if board[i][j] == ' ': 
+                    empty_squares.append((i, j))
+
+        if not empty_squares: 
+            return None
+        
+        return empty_squares[randrange(len(empty_squares))]
+
+    elif level == 2:
+        pos = danger(computer_symbol)
+        if pos is not None and random() < 0.65:
+            return pos
+        pos = danger(player_symbol)
+        if pos is not None and random() < 0.65:
+            return pos
+        elif board[1][1] == ' ' and random() < 0.7:
+            return (1, 1)
+        else:
+            empty_squares = [] 
+            for i in range(3): 
+                for j in range(3): 
+                    if board[i][j] == ' ': 
+                        empty_squares.append((i, j))
+
+            if not empty_squares: 
+                return None
+        
+            return empty_squares[randrange(len(empty_squares))]
+
+    elif level == 3:
+        pos = danger(computer_symbol)
+        if pos is not None and random() < 0.90:
+            return pos
+        pos = danger(player_symbol)
+        if pos is not None and random() < 0.90:
+            return pos
+        elif board[1][1] == ' ':
+            return (1, 1)
+        else:
+            empty_squares = [] 
+            for i in range(3): 
+                for j in range(3): 
+                    if board[i][j] == ' ': 
+                        empty_squares.append((i, j))
+
+            if not empty_squares: 
+                return None
+        
+            return empty_squares[randrange(len(empty_squares))]
+
+    elif level == 4:
+        pos = danger(computer_symbol)
+        if pos is not None:
+            return pos
+        pos = danger(player_symbol)
+        if pos is not None:
+            return pos
+        elif board[1][1] == ' ':
+            return (1, 1)
+        else:
+            corner_squares = [(0, 0), (0, 2), (2, 0), (2, 2)]
+            empty_squares = []
+            for pos in corner_squares:
+                if board[pos[0]][pos[1]] == ' ':
+                    empty_squares.append(pos)
+            if empty_squares:
+                return empty_squares[randrange(len(empty_squares))]
+
+            middle_squares = [(1, 0), (0, 1), (1, 2), (2, 1)]
+            empty_squares = []
+            for pos in middle_squares:
+                if board[pos[0]][pos[1]] == ' ':
+                    empty_squares.append(pos)
+            if empty_squares:
+                return empty_squares[randrange(len(empty_squares))]
+            
+            return None 
---
+++ main.py
@@ -0,0 +1,182 @@
+from time import sleep
+from functions import *
+
+def main():
+
+    stats = {
+    'friend': {'X': 0, 'O': 0, 'round': 1},
+    'computer': {'player': 0, 'computer': 0}
+    }
+
+    prev_mode = None
+
+    print('\nWelcome to Tic Tac Toe!')
+
+    while True:
+
+        print('\n1 - Friend')
+        print('2 - Computer\n')
+
+        game_choice = input_validation('Who do you want to play against?: ', 1, 2)
+
+        if game_choice == 1:
+
+            board = reset_board()
+
+            if prev_mode == 'computer' and (stats['friend']['X'] != 0 or stats['friend']['O'] != 0):
+                reset = input('\nStart a new match?(y/n): ').lower()
+                while reset not in ('y', 'n'):
+                    reset = input('Invalid input, please enter \'y\' for yes or \'n\' for no: ').lower()
+                if reset == 'y':
+                    reset_score(stats)
+                else:
+                    print('\nRetrieving Saved Game Data', end = '')
+                    for i in range(3):
+                        print('.', end='', flush = True)
+                        sleep(0.5)
+                    print('')
+                     
+            print(f'\nRound {stats['friend']['round']}')
+            
+            player = 'O'
+
+            for turn in range(9):
+
+                print_board(board)
+
+                if player == 'X':
+                    player = 'O'
+                else:
+                    player = 'X'
+                print(f'\nPlayer\'s {player} turn')
+
+                while True:
+                    row = input_validation('Enter row: ', 1, 3)
+                    column = input_validation('Enter column: ', 1, 3)
+                    if board[row - 1][column - 1] != ' ':
+                        print('Pick an empty box')
+                        continue
+                    else:
+                        board[row - 1][column - 1] = player
+                        break
+
+                winner = False
+                for i in range(3):
+                    if check_row(board, i):
+                        winner = player
+                        break
+                    elif check_column(board, i):
+                        winner = player
+                        break
+                if not winner:
+                    if check_diagonals(board):
+                        winner = player
+                if winner:
+                    print_board(board)
+                    print(f'\nPlayer {player} won!')
+
+                    if winner == 'X':
+                        stats['friend']['X'] += 1
+                    else:
+                        stats['friend']['O'] += 1
+
+                    print_score(stats['friend']['X'], stats['friend']['O'])
+                    stats['friend']['round'] += 1
+                    break
+            else:
+                print_board(board)
+                print('\nTie!')
+                print_score(stats['friend']['X'], stats['friend']['O'])
+                stats['friend']['round'] += 1
+
+            prev_mode = 'friend'
+
+            end_choice = input('\nDo you want to play another round?(y/n): ').lower()
+            while end_choice not in ('y', 'n'):
+                end_choice = input('Invalid input, please enter \'y\' for yes or \'n\' for no: ').lower()
+
+            if end_choice == 'y':
+                continue 
+            else:
+                print('\nThanks for playing!\n')
+                break 
+
+        elif game_choice == 2:
+
+            board = reset_board()
+
+            print('\n1 - Easy\n2 - Medium\n3 - Hard\n4 - Impossible\n')
+            level = input_validation('Choose the computer\'s AI level: ', 1, 4)
+
+            player_symbol = input('\nChoose your symbol(X/O): ').upper()
+            while player_symbol not in ('X', 'O'):
+                player_symbol = input('\nPlease choose a valid symbol(X/O): ').upper()
+            computer_symbol = 'O' if player_symbol == 'X' else 'X'
+
+            current_player = 'X'
+
+            for turn in range(9):
+
+                print_board(board)
+
+                if current_player == player_symbol:
+                    print('\nYour turn!')
+                else:
+                    print('\nComputer\'s turn', end = '')
+                    for i in range(3):
+                        print('.', end='', flush = True)
+                        sleep(0.5)
+                    print('')
+
+                while True:
+                    if current_player == player_symbol:
+                        row = input_validation('Enter row: ', 1, 3)
+                        column = input_validation('Enter column: ', 1, 3)
+                        if board[row - 1][column - 1] != ' ':
+                            print('Pick an empty box')
+                            continue
+                        else:
+                            board[row - 1][column - 1] = player_symbol
+                            break
+                    else:
+                        pos = computer_move(board, level, player_symbol, computer_symbol)
+                        if pos is None:
+                            break
+                        board[pos[0]][pos[1]] = computer_symbol
+                        break
+
+                winner = False
+                for i in range(3):
+                    if check_row(board, i):
+                        winner = current_player
+                        break
+                    elif check_column(board, i):
+                        winner = current_player
+                        break
+                if not winner:
+                    if check_diagonals(board):
+                        winner = current_player
+                if winner:
+                    print_board(board)
+                    print('You won!') if current_player == player_symbol else print('Game Over')
+                    break
+
+                current_player = computer_symbol if current_player == player_symbol else player_symbol
+            else:
+                print_board(board)
+                print('\nTie!')
+
+            prev_mode = 'computer'
+
+            end_choice = input('\nDo you want to play another round?(y/n): ').lower()
+            while end_choice not in ('y', 'n'):
+                end_choice = input('Invalid input, please enter \'y\' for yes or \'n\' for no: ').lower()
+
+            if end_choice == 'y':
+                continue
+            else:
+                print('\nThanks for playing!\n')
+                break
+
+
+main() 
