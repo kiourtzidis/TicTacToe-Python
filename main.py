1d0b552aa1187abab3dda49980a0c646a06ae8fc
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-01-28 15:57:07 +0200

    Fix UI prompts and friend mode flow

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ def main():
 
     prev_mode = None
 
-    print('\nWelcome to Tic Tac Toe!')
+    print('\nWelcome To Tic Tac Toe!')
 
     while True:
 
         print('\n1 - Friend')
         print('2 - Computer\n')
 
-        game_choice = input_validation('Who do you want to play against?: ', 1, 2)
+        game_choice = input_validation('Who Do You Want To Play Against?: ', 1, 2)
 
         if game_choice == 1:
 
             board = reset_board()
 
-            if prev_mode == 'computer' and (stats['friend']['X'] != 0 or stats['friend']['O'] != 0):
-                reset = input('\nStart a new match?(y/n): ').lower()
+            if prev_mode == 'computer' and stats['friend']['round'] > 1:
+                reset = input('\nStart A New Match?(y/n): ').lower()
                 while reset not in ('y', 'n'):
                     reset = input('Invalid input, please enter \'y\' for yes or \'n\' for no: ').lower()
                 if reset == 'y':
@@ -48,7 +48,7 @@ def main():
                     player = 'O'
                 else:
                     player = 'X'
-                print(f'\nPlayer\'s {player} turn')
+                print(f'\nPlayer\'s {player} Turn')
 
                 while True:
                     row = input_validation('Enter row: ', 1, 3)
@@ -73,7 +73,7 @@ def main():
                         winner = player
                 if winner:
                     print_board(board)
-                    print(f'\nPlayer {player} won!')
+                    print(f'\nPlayer {player} Won!')
 
                     if winner == 'X':
                         stats['friend']['X'] += 1
@@ -98,7 +98,7 @@ def main():
             if end_choice == 'y':
                 continue 
             else:
-                print('\nThanks for playing!\n')
+                print('\nThanks For Playing!\n')
                 break 
 
         elif game_choice == 2:
@@ -120,9 +120,9 @@ def main():
                 print_board(board)
 
                 if current_player == player_symbol:
-                    print('\nYour turn!')
+                    print('\nYour Turn!')
                 else:
-                    print('\nComputer\'s turn', end = '')
+                    print('\nComputer is thinking', end = '')
                     for i in range(3):
                         print('.', end='', flush = True)
                         sleep(0.5)
@@ -158,7 +158,7 @@ def main():
                         winner = current_player
                 if winner:
                     print_board(board)
-                    print('You won!') if current_player == player_symbol else print('Game Over')
+                    print('You Won!') if current_player == player_symbol else print('Game Over')
                     break
 
                 current_player = computer_symbol if current_player == player_symbol else player_symbol
@@ -175,7 +175,7 @@ def main():
             if end_choice == 'y':
                 continue
             else:
-                print('\nThanks for playing!\n')
+                print('\nThanks For Playing!\n')
                 break
 
 
